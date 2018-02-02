c492932f91bbec4ee17a947133500c82716d3e6b
cwy007 <chanweiyan007@gmail.com>
2018-02-02 22:09:56 +0800

    implement dynamic page title with full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def render_user_email(user)
     user.email.split('@').first
   end
+
+  def full_title(page_title = '')
+    base_title = "RailsCasts"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
