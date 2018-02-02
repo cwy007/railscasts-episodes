e3405aa6aaf7b03a10a643068fc0e7ff93c5b753
cwy007 <chanweiyan007@gmail.com>
2018-02-02 16:10:55 +0800

    another version of load_commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,14 @@ class CommentsController < ApplicationController
   end
 
   private
+
+    # def load_commentable
+    #   resource, id = request.path.split('/')[1,2]
+    #   @commentable = resource.singularize.classify.constantize.find(id)
+    # end
+
     def load_commentable
-      resource, id = request.path.split('/')[1,2]
-      @commentable = resource.singularize.classify.constantize.find(id)
+      klass = [Article, Photo].detect { |c| params["#{c.name.underscore}_id"]}
+      @commentable = klass.find(params["#{klass.name.underscore}_id"])
     end
 end
