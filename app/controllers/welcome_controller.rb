ddce3a82c699fab8788e1837e0ea5025b0559677
cwy007 <chanweiyan007@gmail.com>
2018-02-02 08:58:33 +0800

    add partial _flashes.html.erb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    # test flash feature
+    # flash[:notice] = 'notice'
+    # flash[:alert]  = 'alert'
+    # flash[:warning] = 'warning'
   end
 end
